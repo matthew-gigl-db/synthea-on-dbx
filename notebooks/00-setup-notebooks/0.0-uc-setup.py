b0dc1d4bb51e2ab515bc73fb4a8da175920e79d5
matthew-gigl-db <matthew-gigl-db>
2024-05-25 23:18:26 +0000

    fixed markdown

--- notebooks/00-setup-notebooks/0.0-uc-setup.py
+++ notebooks/00-setup-notebooks/0.0-uc-setup.py
@@ -6,7 +6,7 @@
 # MAGIC
 # MAGIC It's recommended to use a catalog name that you have read and write access to, or to create a new catalog (assuming that you have that permissoin in UC) to you'll be able to use to run the demo.  A brand new catalog is not required.  Please note that there is not a default setting for catalog, therefore you will need to supply a value in the Databricks widget.  You're welcome to create your own schema name in your catalog of choice.  The default schema name is "synthea" since we'll be generating synthetic files using the Synthea package.  You may need to change the schema name if more than one person in your organization is running this demo at the same time.  
 # MAGIC
-# MAGIC Since Unity Catalog uses a three level name space, the volume name "synthetic_files_raw" is techincally unique if either the catalog name or the schema name is unique.  For simplicity we'll be using a managed volume, though external volumes are also an option.  Our volume will have the path:  "/Volumes/<catalog_name>/<schema_name>/synthetic_files_raw/" 
+# MAGIC Since Unity Catalog uses a three level name space, the volume name "synthetic_files_raw" is techincally unique if either the catalog name or the schema name is unique.  For simplicity we'll be using a managed volume, though external volumes are also an option.  Our volume will have the path:  "/Volumes/\<catalog_name\>/\<schema_name\>/synthetic_files_raw/" 
 
 # COMMAND ----------
 
