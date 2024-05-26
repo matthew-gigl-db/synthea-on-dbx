d6af10b54755a783b7a25fe6397c6cb1e57eb7f0
matthew-gigl-db <matthew-gigl-db>
2024-05-26 08:14:59 +0000

    first task completed

--- workflows/synthea-on-dbx-create-workflow.py
+++ workflows/synthea-on-dbx-create-workflow.py
@@ -14,6 +14,7 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 
+# DBTITLE 1,Set Databricks Widgets
 dbutils.widgets.text("catalog_name", "")
 dbutils.widgets.text("schema_name", "synthea")
 dbutils.widgets.text("instance_pool_id", "", "Optional Instance Pool ID for the Cluster Spec")
@@ -21,9 +22,7 @@ dbutils.widgets.text("node_type_id", "i3.xlarge", "Node Type Id, Required if Ins
 
 # COMMAND ----------
 
-# DBTITLE 1,Input Catalog and Schema Name.
-# catalog_name = input("Catalog name (for example, main) for the job to write files into the schema's Volume: ")
-# schema_name = input("Schema name (for example, my_schema) that the Volume will be created in: ")
+# DBTITLE 1,Get Widget Inputs
 catalog_name = dbutils.widgets.get("catalog_name")
 schema_name = dbutils.widgets.get("schema_name")
 instance_pool_id = dbutils.widgets.get("instance_pool_id")
@@ -31,7 +30,7 @@ node_type_id = dbutils.widgets.get("node_type_id")
 
 # COMMAND ----------
 
-# DBTITLE 1,Catalog Schema Volume
+# DBTITLE 1,File Destination Information
 print(
 f"""
 Based on user input's the job will write files into this catalog.schema's Volume:
@@ -49,28 +48,27 @@ Please note that is the catalog, schema, or Volume do not exist, the workflow no
 
 # DBTITLE 1,Databricks SDK workspace initialization
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.service.jobs import Source, Task, NotebookTask, TaskEmailNotifications, TaskNotificationSettings, WebhookNotifications, RunIf
+from databricks.sdk.service.jobs import Source, Task, NotebookTask, TaskEmailNotifications, TaskNotificationSettings, WebhookNotifications, RunIf, QueueSettings, JobParameter, JobRunAs
 from databricks.sdk.service.compute import ClusterSpec, DataSecurityMode, RuntimeEngine
 
 w = WorkspaceClient()
 
 # COMMAND ----------
 
+# DBTITLE 1,Get Current User and Spark Version
 current_user = w.current_user.me()
 latest_lts_version = w.clusters.select_spark_version(latest=True, long_term_support=True)
 
 # COMMAND ----------
 
-# current_user.as_dict()
-
-# COMMAND ----------
-
+# DBTITLE 1,Synthetic Data Deneration Job Inputs
 job_name = current_user.name.family_name.lower() + "-" + current_user.id + "-synthea-data-generation"
 job_cluster_key = current_user.name.family_name.lower() + "-" + current_user.id + "-synthea-data-gen"
 job_description = f"Job to generate synthetic data for {current_user.user_name} in /Volumes/{catalog_name}/{schema_name}/synthetic_files_raw/ using the synthea library jar."
 
 # COMMAND ----------
 
+# DBTITLE 1,Print Job Inputs
 print(
 f"""
 Current user: {current_user.user_name}
@@ -90,6 +88,7 @@ Note that node_type_id will only be used if an instance_pool_id is not set.
 
 # COMMAND ----------
 
+# DBTITLE 1,Job Cluster Specification Creation
 if instance_pool_id == "": 
   cluster_spec = JobCluster(
     job_cluster_key = job_cluster_key
@@ -136,31 +135,25 @@ else:
 
 # COMMAND ----------
 
-# DBTITLE 1,Check if a job with the same name exists
+# DBTITLE 1,Existing Jobs List and Deletion
 jobs_list = w.jobs.list(
-  expand_tasks=False
+  expand_tasks = False
   ,name = job_name
 )
 jobs_list = [job.as_dict() for job in jobs_list]
-print(jobs_list)
+print(jobs_list, "\n")
 if len(jobs_list) == 0: 
-  print("No jobs found. Creating a new job...")
+  print("No jobs found. Proceed with creating a new job...")
 else:
-  print("One or more jobs with the same name already exists. Deleting the jobs...")
+  print("One or more jobs with the same name already exists. Deleting the jobs...\n\n")
   for i in range(0,len(jobs_list)):
-    print(f"Deleting job {jobs_list[i].get('job_id')}")
+    print(f"Deleting job {jobs_list[i].get('job_id')}\n")
     w.jobs.delete(jobs_list[i].get("job_id"))
-  print("All jobs with the same name have been deleted. Creating a new job...")
+  print("All jobs with the same name have been deleted. Proceed with creating a new job...")
 
 # COMMAND ----------
 
-
-# job_name            = input("Some short name for the job (for example, my-job): ")
-# description         = input("Some short description for the job (for example, My job): ")
-# existing_cluster_id = input("ID of the existing cluster in the workspace to run the job on (for example, 1234-567890-ab123cd4): ")
-# notebook_path       = input("Workspace path of the notebook to run (for example, /Users/someone@example.com/my-notebook): ")
-# task_key            = input("Some key to apply to the job's tasks (for example, my-key): ")
-
+# DBTITLE 1,Create Synthea Data Generation Workflow
 print("Attempting to create the job. Please wait...\n")
 
 j = w.jobs.create(
@@ -188,71 +181,22 @@ j = w.jobs.create(
     )
   ]
   ,job_clusters = [cluster_spec]
+  ,queue = QueueSettings(enabled = True)
+  ,parameters = [
+    JobParameter(
+      name = "catalog_name"
+      ,default = catalog_name
+      ,value = catalog_name
+    )
+    ,JobParameter(
+      name = "schema_name"
+      ,default = schema_name
+      ,value = schema_name
+    )
+  ]
+  ,run_as = JobRunAs(
+    user_name = current_user.user_name
+  )
 )
 
-print(f"View the job at {w.config.host}/#job/{j.job_id}\n")
-
-
-# COMMAND ----------
-
-"job_clusters": [
-    {
-      "job_cluster_key": "mg-synthea-data-gen",
-      "new_cluster": {
-        "cluster_name": "",
-        "spark_version": "14.3.x-scala2.12",
-        "spark_conf": {
-          "spark.master": "local[*, 4]",
-          "spark.databricks.cluster.profile": "singleNode"
-        },
-        "custom_tags": {
-          "ResourceClass": "SingleNode"
-        },
-        "spark_env_vars": {
-          "JNAME": "zulu17-ca-amd64"
-        },
-        "#########comment": "instance_pool_id and driver_instance_pool_id are specific to the workspace",
-        "instance_pool_id": "0727-104344-hauls13-pool-uftxk0r6", 
-        "driver_instance_pool_id": "0727-104344-hauls13-pool-uftxk0r6",
-        "data_security_mode": "SINGLE_USER",
-        "runtime_engine": "STANDARD",
-        "num_workers": 0
-      }
-    }
-  ],
-  "queue": {
-    "enabled": true
-  },
-  "parameters": [
-    {
-      "name": "catalog_name",
-      "default": "mgiglia"
-    },
-    {
-      "name": "schema_name",
-      "default": "synthea"
-    }
-  ],
-  "run_as": {
-    "user_name": "matthew.giglia@databricks.com"
-  }
-
-# COMMAND ----------
-
-    {
-      "task_key": "synthea_set_up_check",
-      "run_if": "ALL_SUCCESS",
-      "notebook_task": {
-        "notebook_path": "/Workspace/Users/matthew.giglia@databricks.com/db-nosql/00-setup-notebooks/set-up-check",
-        "source": "WORKSPACE"
-      },
-      "job_cluster_key": "mg-synthea-data-gen",
-      "timeout_seconds": 0,
-      "email_notifications": {},
-      "notification_settings": {
-        "no_alert_for_skipped_runs": false,
-        "no_alert_for_canceled_runs": false,
-        "alert_on_last_attempt": false
-      },
-      "webhook_notifications": {}
-    }
+print(f"Job created successfully. Job ID: {j.job_id}")
