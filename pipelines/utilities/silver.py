24904c09749f6b870dfdfb740da04d167999940e
matthew-gigl-db <matthew-gigl-db>
2025-05-12 03:23:06 +0000

    fix comma

--- pipelines/utilities/silver.py
+++ pipelines/utilities/silver.py
@@ -4,8 +4,8 @@ from spark.sql.functions import col
 
 class Silver:
   def __init__(self, spark: SparkSession, table_definition: dict):
-    self.spark = spark
-    ,self.table_definition = table_definition
+    self.spark = spark,
+    self.table_definition = table_definition
     """
     The Silver class is responsible for transforming data from the bronze table to the silver table.
     
