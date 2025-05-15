0ee46ab2b81b88ee281ac05ee90ad3284ef924d0
matthew-gigl-db <matthew-gigl-db>
2025-05-15 16:54:29 +0000

    add pipeline definition to resources add sql comment

--- pipelines/transformations/encounters.sql
+++ pipelines/transformations/encounters.sql
@@ -1,10 +1,10 @@
--- this is a comment 
+-- Create or Refresh Target Streaming Table 
 CREATE OR REFRESH STREAMING TABLE encounters (
- CONSTRAINT encounter_patient_id_null EXPECT (PATIENT IS NOT NULL)
+	CONSTRAINT encounter_patient_id_null EXPECT (patient_id IS NOT NULL) ON VIOLATION DROP ROW
 )
 COMMENT 'Patient encounters.' 
 TBLPROPERTIES (
-	'quality' = 'bronze'
+	'quality' = 'silver'
 	,'delta.enableChangeDataFeed' = 'true'
 	,'delta.enableDeletionVectors' = 'true'
 	,'delta.enableRowTracking' = 'true'
@@ -16,3 +16,16 @@ KEYS (encounter_id)
 SEQUENCE BY file_metadata.file_modification_time
 COLUMNS * EXCEPT (file_metadata, ingest_time)
 STORED AS SCD TYPE 1;
+
+CREATE OR REFRESH STREAMING TABLE encounters_quarantine (
+	CONSTRAINT encounter_patient_id_not_null EXPECT (patient_id IS NULL) ON VIOLATION DROP ROW
+)
+COMMENT 'Patient encounters quarantine table where records should be reviewed.' 
+TBLPROPERTIES (
+	'quality' = 'bronze'
+	,'delta.enableChangeDataFeed' = 'true'
+	,'delta.enableDeletionVectors' = 'true'
+	,'delta.enableRowTracking' = 'true'
+)
+AS SELECT * FROM STREAM (encounters_stage);
+
